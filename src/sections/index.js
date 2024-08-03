ecbdb0bc51e1ab2bbf95d83d456dd9a7f9936808
Adil_01_st <92965999+Adil-01-st@users.noreply.github.com>
2024-08-03 22:56:36 +0200

    Sports shoe review Nike site created with Tailwind

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import Nav from '../components/Nav'
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+
+export {
+    Hero,
+    Nav,
+    CustomerReviews,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
